fcd21d06006494a134972a2d2a06abbd673a12d7
8FManBelgium <bausmans@gmail.com>
2025-06-30 12:53:42 +0200

    ip adress tracker toegevoegd

--- script.js
+++ script.js
@@ -33,3 +33,16 @@ function getRandomMessage() {
 
     ];
 }
+
+const ipEl = document.getElementById("ip");
+const countryEl = document.getElementById("country");
+const cityEl = document.getElementById("city");
+
+fetch("https://ip-api.com/json/?fields=61439")
+.then((res) => res.json())
+.then((res)=> {
+    ipEl.textContent = res.query;
+    countryEl.textContent = res.country;
+    cityEl.textContent = res.city;
+    
+});
