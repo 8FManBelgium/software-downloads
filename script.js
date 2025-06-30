023b441ef6476f5f9bab0ad3c6a0ffeb557baa41
8FManBelgium <bausmans@gmail.com>
2025-06-30 13:04:03 +0200

    http aangepast

--- script.js
+++ script.js
@@ -38,11 +38,11 @@ const ipEl = document.getElementById("ip");
 const countryEl = document.getElementById("country");
 const cityEl = document.getElementById("city");
 
-fetch("https://ip-api.com/json/?fields=61439")
+fetch("http://ip-api.com/json/?fields=61439")
 .then((res) => res.json())
 .then((res)=> {
     ipEl.textContent = res.query;
     countryEl.textContent = res.country;
     cityEl.textContent = res.city;
-    
+
 });
