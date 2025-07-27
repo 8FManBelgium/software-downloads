6cfae0111ac6bd56c23810b76748ed97d9bdeb1d
8FManBelgium <bausmans@gmail.com>
2025-07-27 13:25:00 +0200

    terug gezet

--- script.js
+++ script.js
@@ -16,6 +16,33 @@ let d = new Date();
 time.innerHTML = d.toLocaleTimeString();
 }, 1000);
 
+const button = document.getElementById("button");
+const toasts = document.getElementById("toasts");
+const message =[
+    'Mail Program Opend Zo',
+    'Nog even geduld',
+    'Mail Programma word geopend',
+    'Mail Programma word geopend, even geduld',
+]
+
+button.addEventListener("click", () => createNotification())
+
+function createNotification() {
+    const notif = document.createElement("div");
+    notif.classList.add("toast");
+    notif.innerText = getRandomMessage();
+
+    toasts.appendChild(notif);
+
+    setTimeout(() => {
+        notif.remove();
+    }, 4000);
+}
+function getRandomMessage() {
+    return message[Math.floor(Math.random() * message.length)
+
+    ];
+}
 
 
 
